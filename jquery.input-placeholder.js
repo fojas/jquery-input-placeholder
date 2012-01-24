245fb4ba09a516c316e1b630abd423e40b3b80a9
Fojas <developer@fojasaur.us>
2012-01-24 14:41:50 -0600

    only clone one elem at a time

--- jquery.input-placeholder.js
+++ jquery.input-placeholder.js
@@ -4,25 +4,27 @@
       var _hasPlaceHolder = ('placeholder' in document.createElement('input'));
       return function(){
         if(!_hasPlaceHolder){
-          var orig = this,
-          clon = orig.clone();
-          clon.attr('readonly','readonly');
-          clon.val(clon.attr('placeholder'))
-            .removeAttr('placeholder')
-            .removeAttr('name')
-            .insertAfter(orig);
-          orig.hide().blur(function(){
-            if(this.value === ''){
-              orig.hide();
-              clon.show();
-            } 
-          })
-          clon.click(function(){
-            orig.show().focus();
-            clon.hide();
+          this.each(function(){
+            var orig = $(this),
+            clon = orig.clone();
+            clon.attr('readonly','readonly');
+            clon.val(clon.attr('placeholder'))
+              .removeAttr('placeholder')
+              .removeAttr('name')
+              .insertAfter(orig);
+            orig.hide().blur(function(){
+              if(this.value === ''){
+                orig.hide();
+                clon.show();
+              } 
+            })
+            clon.click(function(){
+              clon.hide();
+              orig.show().focus();
+            });
           });
         };
-        return orig;
+        return this;
       };
     }()
   });
